cbbe09d1ddfead1fb13d7c0c25856aa0ddf45186
DoxxMulder <fugcaptaindefect@gmail.com>
2021-05-04 23:17:04 -0700

    Various optimization

--- bot.py
+++ bot.py
@@ -31,6 +31,31 @@ changes_list = []
 in_position = False
 bought_at = 0.0
 running_total = 0.0
+hold_counter = 0
+
+def buyBtc(quantity, price):
+    try:
+        bTxt ="Bought {:.6f} BTC at ${:,.2f}/BTC for a total of ${:,.2f}"
+        print(bTxt.format(float(quantity * float(price)), float(price), quantity * float(price)))
+        bought_at = float(price)
+        running_total -= quantity * float(price)
+        print("Running total of gain/loss: ${:,.2f}".format(running_total))
+
+    except Exception as e:
+        return False
+    
+    return True
+
+def sellBtc(quantity, price):
+    try:
+        sTxt = "Sold {:.6f} BTC at ${:,.2f}/BTC for a total of ${:,.2f}" 
+        print(sTxt.format(float(quantity), float(price), float(quantity * float(price))))
+        running_total += quantity * float(price)
+        print("Running total of gain/loss: ${:,.2f}".format(running_total))
+    except Exception as e:
+        return False
+    
+    return True
 
 while(True):
 
@@ -43,38 +68,59 @@ while(True):
     if len(changes_list) > RSI_PERIOD:
         np_closes = numpy.array(changes_list)
         rsi = talib.RSI(np_closes, RSI_PERIOD)
+        '''
         print("All RSIs calculated so far:")
         print(rsi)
+        '''
         last_rsi = rsi[-1]
         print("Current rsi is {:2f}".format(float(last_rsi)))
 
         if last_rsi > RSI_OVERBOUGHT:
             if in_position and (bought_at < float(sell_price.amount)):
                 print("Sell!")
+                '''
                 sTxt = "Sold {:.6f} BTC at ${:,.2f}/BTC for a total of ${:,.2f}" 
                 print(sTxt.format(float(TRADE_QUANTITY), float(sell_price.amount), float(TRADE_QUANTITY * float(sell_price.amount))))
                 running_total += TRADE_QUANTITY * float(sell_price.amount())
                 print("Running total of gain/loss: ${:,.2f}".format(running_total))
                 in_position = False
-            elif in_position and (bought_at > float(sell_price.amount)):
-                print("Overbought, but selling would cause a loss")
+                '''
+                sell_succeeded = sellBtc(TRADE_QUANTITY, sell_price.amount)
+                if sell_succeeded:
+                    in_position = False
+            elif in_position and (bought_at >= float(sell_price.amount)):
+                if hold_counter == 5:
+                    hold_counter = 0
+                    print("Overbought - selling despite loss due to hold timer")
+                    sell_succeeded = sellBtc(TRADE_QUANTITY, sell_price.amount)
+                    if sell_succeeded:
+                        in_position = False
+                else:
+                    hold_counter += 1
+                    print("Overbought, but selling would cause a loss. Count: {}".format(hold_counter))
             else:
                 print("Overbought, but nothing to sell")
 
         if last_rsi < RSI_OVERSOLD:
+            hold_counter = 0
             if in_position:
                 print("Oversold, but already own it")
             else:
                 print("Buy!")
+                '''
                 bTxt ="Bought {:.6f} BTC at ${:,.2f}/BTC for a total of ${:,.2f}"
                 print(bTxt.format(float(TRADE_QUANTITY * float(buy_price.amount)), float(buy_price.amount), TRADE_QUANTITY * float(buy_price.amount)))
                 bought_at = float(buy_price.amount)
                 running_total -= TRADE_QUANTITY * float(buy_price.amount)
                 print("Running total of gain/loss: ${:,.2f}".format(running_total))
                 in_position = True
+                '''
+                buy_succeeded = buyBtc(TRADE_QUANTITY, buy_price.amount)
+                if buy_succeeded:
+                    in_position = True
     
     #Print current BTC price + percent change
-    print(" "*25 + '\nBitcoin is ${:,.2f} \nPercent change in last {} seconds: {:.6f}%'.format(float(buy_price.amount), CYCLE_TIME, percentage_gainloss))
+    print(" "*25 + '\nBitcoin is ${:,.2f} \nPercent change in last {} seconds: {:.3f}%'.format(float(buy_price.amount), CYCLE_TIME, percentage_gainloss))
     '''
     #Sell, if within sell threshold
     if(float(sell_price.amount) > user_limit_order):
